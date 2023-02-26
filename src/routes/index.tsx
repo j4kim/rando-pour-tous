7c8404cbb1de7a3ba6cd0d2673588f510d4ff111
Joaquim Perez <jivkim@gmail.com>
2023-02-26 22:23:53 +0100

    change title

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Rando pour tous</Title>
+      <h1>Rando pour tous</h1>
       <Counter />
       <p>
         Visit{" "}
