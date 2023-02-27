8dced572082310533ded0ce678e6579ed58afff8
Joaquim Perez <jivkim@gmail.com>
2023-02-27 22:53:44 +0100

    simplify index and remove counter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,6 @@
-import { Title } from "solid-start";
-import Counter from "~/components/Counter";
-
 export default function Home() {
   return (
     <main>
-      <Title>Rando pour tous</Title>
-      <h1>Rando pour tous</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
