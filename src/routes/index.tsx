9305f24dd64a7164b65efd1341914a8e4418ae44
Joaquim Perez <jivkim@gmail.com>
2023-02-27 23:48:49 +0100

    download content has routeData

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,27 @@
+import { For } from "solid-js";
+import { createRouteData, useRouteData } from "solid-start";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: "nslgdnzpa24d",
+  accessToken: "-YD620bRfsUnApfr8fO4xG-v_H4iz5cpmmerqFBBUEQ",
+});
+
+export function routeData() {
+  return createRouteData(async () => {
+    const entries = await client.getEntries();
+    const items = entries.items
+    const sections = items.filter(i => i.sys.contentType.sys.id === 'section')
+    return sections
+  });
+}
+
 export default function Home() {
-  return (
-    <main>
-    </main>
-  );
+  const sections = useRouteData<typeof routeData>();
+
+  return <main>
+    <For each={sections()}>
+      {(section) => <p>{JSON.stringify(section.fields)}</p>}
+    </For>
+  </main>;
 }
