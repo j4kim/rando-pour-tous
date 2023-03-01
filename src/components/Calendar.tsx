df21bc7b22fe384e07cb6ef740ecb78b97d2cdfc
Joaquim Perez <jivkim@gmail.com>
2023-03-01 15:52:39 +0100

    add full calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,3 +1,22 @@
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import listPlugin from "@fullcalendar/list";
+import { onMount } from "solid-js";
+
 export default function () {
-  return <div>Calendar</div>;
+  let cal;
+
+  onMount(() => {
+    new Calendar(cal, {
+      plugins: [dayGridPlugin, listPlugin],
+      initialView: "dayGridMonth",
+      headerToolbar: {
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,listWeek",
+      },
+    }).render();
+  });
+
+  return <div ref={cal} style="margin: 1em auto; font-size:16px"></div>;
 }
