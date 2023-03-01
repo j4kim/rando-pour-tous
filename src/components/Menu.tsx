a5c8be6e983be8986735ffed511d61c3eb178980
Joaquim Perez <jivkim@gmail.com>
2023-03-01 14:21:44 +0100

    menu layout

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,12 @@
 import { For } from "solid-js";
+import theme from "~/theme";
 import { MenuProps } from "~/types";
 import "./Menu.scss";
 
 export default function (props: MenuProps) {
   const sections = props.sections;
   return (
-    <nav>
+    <nav style={{ "background-color": theme.green, color: "white" }}>
       <ul>
         <For each={sections}>
           {(s) => (
