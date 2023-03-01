8e1c6aed418d6779ca5adbb616e5faa4860d75d2
Joaquim Perez <jivkim@gmail.com>
2023-03-01 22:10:16 +0100

    add photoswipe

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,16 +1,37 @@
-import { For } from "solid-js";
+import { For, onMount } from "solid-js";
 import { assets } from "~/store";
 import "./Gallery.scss";
+import PhotoSwipeLightbox from "photoswipe/lightbox";
+import "photoswipe/style.css";
 
 export default function () {
+  let gallery;
+
+  onMount(() => {
+    const lightbox = new PhotoSwipeLightbox({
+      gallery: gallery,
+      children: "a",
+      pswpModule: () => import("photoswipe"),
+    });
+    lightbox.init();
+  });
+
   return (
-    <div class="gallery">
+    <div ref={gallery} class="gallery">
       <For each={assets()}>
         {(asset) => (
-          <img
-            src={`${asset.fields.file.url}?w=160&h=160&fit=fill`}
-            alt={asset.fields.title}
-          />
+          <a
+            href={asset.fields.file.url}
+            data-pswp-width={asset.fields.file.details.image.width}
+            data-pswp-height={asset.fields.file.details.image.height}
+            data-cropped="true"
+            target="_blank"
+          >
+            <img
+              src={`${asset.fields.file.url}?w=160&h=160`}
+              alt={asset.fields.title}
+            />
+          </a>
         )}
       </For>
     </div>
