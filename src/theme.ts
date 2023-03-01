17b6ac02d885b9611808f1c928e05fa83c60f114
Joaquim Perez <jivkim@gmail.com>
2023-03-01 13:04:42 +0100

    a new theme.ts file to configure colors

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+export default {
+  gold: "hsl(51, 48%, 60%)", // #c9bb67
+  lightGold: "hsl(51, 55%, 82%)", // #eae2b7
+  green: "hsl(164, 52%, 32%)", // #277c66
+  darkGreen: "hsl(163, 45%, 26%)", // #24604f
+  blue: "hsl(241, 36%, 38%)", // #3f3e83
+  darkBlue: "hsl(240, 25%, 30%)", // #393960
+  brick: "hsl(1, 55%, 60%)", // #d16361
+}
